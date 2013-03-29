fd2622f2de903c70c23da25bcee34e65cd4bb754
Tim Smart <tim@fostle.com>
2013-03-29 19:40:59 +1300

    Clean up and fixes
    Signed-off-by: Tim Smart <tim@fostle.com>

--- index.js
+++ index.js
@@ -30,38 +30,38 @@ var url      = require('url')
 var next_ext = require('./lib/next')
 var events   = require('events')
 
-var noop = function () {};
+var noop = function () {}
 
 // The Router prototype
 var Router = function Router (server, config) {
-  var self = this;
+  var self = this
 
   config || (config = {})
 
   this.env     = config.env || process.env.NODE_ENV || 'development'
-  this.routes  = [];
+  this.routes  = []
   this.headers = config.headers || { Server : 'node.js' }
-  this.next    = next_ext;
+  this.next    = next_ext
 
-  next_ext.setDefaultHeaders(this.headers);
+  next_ext.setDefaultHeaders(this.headers)
 
   server.on('request', function (request, response) {
     if (self.routes.length === 0) {
       // We got nothing to work with :(
-      response.writeHead(404);
-      return response.end();
+      response.writeHead(404)
+      return response.end()
     }
 
     // Prevent crashes on HEAD requests
     if (request.method === 'HEAD') {
-      var old_end = response.end;
+      var old_end = response.end
 
       response.write = function write(data) {
-        return data.length;
-      };
+        return true
+      }
       response.end = function end() {
-        return old_end.call(response);
-      };
+        return old_end.call(response)
+      }
     }
 
     var i = 0
@@ -74,7 +74,7 @@ var Router = function Router (server, config) {
         if (err) {
           while (self.routes[i]) {
             if (self.routes[i].errorhandler) {
-              return self.routes[i].handle(error, request, response, next);
+              return self.routes[i].handle(request, response, next, err)
             }
 
             ;++i
@@ -83,7 +83,7 @@ var Router = function Router (server, config) {
           return next(err)
         }
 
-        self.routes[i].handle(request, response, next);
+        self.routes[i].handle(request, response, next)
       } else {
         if (err) {
           self.emit('error', err)
@@ -95,24 +95,24 @@ var Router = function Router (server, config) {
           self.emit('error', err)
         }
       }
-    };
-    next.__proto__ = next_ext;
-    next.response = response;
+    }
+    next.__proto__ = next_ext
+    next.response = response
     next.request  = request
 
     // Get the party started
-    self.routes[i].handle(request, response, next);
+    self.routes[i].handle(request, response, next)
   })
 
   server.on('error', function (err) {
     self.emit('error', err)
   })
-};
+}
 
-Router.next = next_ext;
+Router.next = next_ext
 
 // Extend http.Server
-Router.prototype.__proto__ = events.EventEmitter.prototype;
+Router.prototype.__proto__ = events.EventEmitter.prototype
 
 // Configure
 Router.prototype.configure = function configure (env, config) {
@@ -128,78 +128,74 @@ Router.prototype.configure = function configure (env, config) {
   return this
 }
 
-// All requests proxy through this method
-Router.prototype._onRequest = function _onRequest(request, response) {
-};
-
 // Low level api to manually create Route
 Router.prototype.createRoute = function createRoute(config) {
-  return new Route(this, config);
-};
+  return new Route(this, config)
+}
 
 // Set a route as a parallel one
 Router.prototype.parallel = function parallel() {
   var route = new Route(this, {
     parallel: true
-  });
-  this.routes.push(route);
-  return route;
-};
+  })
+  this.routes.push(route)
+  return route
+}
 
 // Proxy method to create a new route with 'bind'
 Router.prototype.bind = function bind() {
   var route = new Route(this, {
     catch_all: true
-  });
-  this.routes.push(route);
-  return route.bind.apply(route, arguments);
-};
+  })
+  this.routes.push(route)
+  return route.bind.apply(route, arguments)
+}
 
 // Creates and proxies a method to the Route prototype
 Router.prototype._proxyMethod = function _proxyMethod(verb, args) {
-  var route = new Route(this);
-  this.routes.push(route);
-  return route[verb].apply(route, args);
-};
+  var route = new Route(this)
+  this.routes.push(route)
+  return route[verb].apply(route, args)
+}
 
 // Proxy method to create a new route with 'all'
-Router.prototype.all = function all() {
+Router.prototype.all = function all () {
   return this._proxyMethod('all', arguments)
-};
+}
 
 // Proxy method to create a new route with 'get'
-Router.prototype.get = function get() {
+Router.prototype.get = function get () {
   return this._proxyMethod('get', arguments)
-};
+}
 
 // Proxy method to create a new route with 'post'
-Router.prototype.post = function post() {
+Router.prototype.post = function post () {
   return this._proxyMethod('post', arguments)
-};
+}
 
 // Proxy method to create a new route with 'put'
-Router.prototype.put = function put() {
+Router.prototype.put = function put () {
   return this._proxyMethod('put', arguments)
-};
+}
 
 // Proxy method to create a new route with 'patch'
-Router.prototype.patch = function patch() {
+Router.prototype.patch = function patch () {
   return this._proxyMethod('patch', arguments)
-};
+}
 
 // Proxy method to create a new route with 'del'
-Router.prototype.del = function del() {
+Router.prototype.delete = function _delete () {
   return this._proxyMethod('del', arguments)
-};
+}
 
 // Proxy method to create a new route with 'options'
 Router.prototype.options = function options() {
   return this._proxyMethod('options', arguments)
-};
+}
 
 // The Route prototype
 var Route = function Route(router, config) {
-  this.router = router;
+  this.router = router
 
   // The default config
   this.parallel     = false
@@ -215,14 +211,17 @@ var Route = function Route(router, config) {
     PATCH: [],
     DELETE: [],
     OPTIONS: []
-  };
+  }
+
+  this.errorhandlers = []
 
   // The processing layers
-  this.layers = [];
+  this.layers = []
+  this.length = 0
 
   // Proxy the config over
   if (config) {
-    var self = this;
+    var self = this
     var keys = Object.keys(config)
 
     for (var i = 0, il = keys.length; i < il; i++) {
@@ -233,169 +232,197 @@ var Route = function Route(router, config) {
 
 // The proxy for routes. Check for a match, then pass through
 // the other stuff in order.
-Route.prototype.handle = function handle(request, response, callback) {
+Route.prototype.handle = function handle(request, response, callback, error) {
   if (this.parallel && callback) {
-    callback();
-    callback = noop;
+    callback()
+    callback = noop
   }
 
   var method = request.method,
       self   = this,
       match  = false,
-      next,
-      i      = 0;
+      i      = 0
 
   if (method === 'HEAD') {
     // HEAD requests aren't allowed a body, but are
     // treated like a GET request
-    method = 'GET';
+    method = 'GET'
   }
 
   if (this.catch_all === true) {
-    match = true;
+    match = true
   } else if (this.parse) {
     match = this.parse(request)
   } else {
     var lower_path = request.url.toLowerCase(),
         j          = 0,
         length     = this.table[method].length,
-        route;
+        route
 
     for (; j < length; j++) {
-      route = this.table[method][j];
+      route = this.table[method][j]
       if (route instanceof RegExp) {
-        var temp_match;
+        var temp_match
         if (temp_match = lower_path.match(route)) {
           // TODO: Remove shift. Use queue?
-          temp_match.shift();
+          temp_match.shift()
           // Keep a reference to the last regexp match
-          match = temp_match;
+          match = temp_match
         }
       } else if (route === lower_path) {
-        match = true;
+        match = true
       }
     }
   }
 
-  if (match) {
+  if (match && 1 === self.length) {
+    function next (err) {
+      return callback(err)
+    }
+
+    var layer = self.layers[0]
+
+    // Add __proto__ methods.
+    next.__proto__ = next_ext
+    next.response  = response
+    next.request   = request
+
+    // Matches
+    if ('object' === typeof match) {
+      next.match = match
+    } else {
+      next.match = null
+    }
+
+    if (self.errorhandler) {
+      layer(error, request, response, next)
+    } else {
+      layer(request, response, next)
+    }
+  } else if (match) {
     // We have a match! Time to go through the processing layers
-    next = function next(err) {
+    function next (err) {
       if (err) {
         return callback(err)
       }
 
       var args,
-          layer = self.layers[i++];
+          layer = self.layers[i++]
 
       if (layer) {
-        args = [request, response, next];
-
-        // Regex match
-        if ('object' === typeof match) {
-          next.match = match
-        } else {
-          next.match = null
-        }
-
         // Catch layer errors
         try {
           layer(request, response, next)
         } catch (error) {
-          next(error);
+          next(error)
         }
       } else {
-        return callback();
+        return callback()
       }
-    };
+    }
 
     // Add __proto__ methods.
-    next.__proto__ = next_ext;
-    next.response  = response;
+    next.__proto__ = next_ext
+    next.response  = response
     next.request   = request
 
+    // Matches
+    if ('object' === typeof match) {
+      next.match = match
+    } else {
+      next.match = null
+    }
+
     // Start the processing madness
-    next();
+    next()
   } else if (callback) {
-    callback();
+    callback()
   }
 
-  return this;
-};
+  return this
+}
 
 // Private: Checks to see whether we can use the route
 Route.prototype._checkRoute = function _checkRoute(route) {
   if (route instanceof RegExp) {
-    return true;
+    return true
   } else if (typeof route === 'string') {
-    return true;
+    return true
   }
-  log('Warning: The route "' + route + '" was of unrecognised type.');
-  return false;
-};
+  log('Warning: The route "' + route + '" was of unrecognised type.')
+  return false
+}
 
 // bind: A simple processing layer
 Route.prototype.bind = function bind(fn) {
   if (typeof fn === 'function') {
-    this.layers.push(fn);
+    if (this.errorhandler) {
+      log('Warning: error handler routes only can have one bind()')
+      return this
+    }
 
-    if (4 === fn.length) {
+    this.layers.push(fn)
+    ;++this.length
+
+    if (this.catch_all && 4 === fn.length && 1 === this.length) {
       this.errorhandler = true
+      this.errorhandlers.push(fn)
     }
   } else {
-    log('Warning: bind only accepts functions.');
+    log('Warning: bind only accepts functions.')
   }
-  return this;
-};
+  return this
+}
 
 // all: Serves all types of request
 Route.prototype.all = function all(route) {
   if (this._checkRoute(route)) {
-    this.table.GET.push(route);
-    this.table.POST.push(route);
-    this.table.PUT.push(route);
-    this.table.PATCH.push(route);
-    this.table.DELETE.push(route);
-    this.table.OPTIONS.push(route);
+    this.table.GET.push(route)
+    this.table.POST.push(route)
+    this.table.PUT.push(route)
+    this.table.PATCH.push(route)
+    this.table.DELETE.push(route)
+    this.table.OPTIONS.push(route)
   }
-  return this;
-};
+  return this
+}
 
 // Shortcut to add route
 Route.prototype._addRoute = function _addRoute(verb, route) {
   if (this._checkRoute(route)) {
-    this.table[verb].push(route);
+    this.table[verb].push(route)
   }
-  return this;
-};
+  return this
+}
 
 // get: Matches against get requests
-Route.prototype.get = function get(route) {
-  return this._addRoute('GET', route);
-};
+Route.prototype.get = function get (route) {
+  return this._addRoute('GET', route)
+}
 
 // post: Matches against post requests
-Route.prototype.post = function post(route) {
-  return this._addRoute('POST', route);
-};
+Route.prototype.post = function post (route) {
+  return this._addRoute('POST', route)
+}
 
 // put: Matches against put requests
-Route.prototype.put = function put(route) {
-  return this._addRoute('PUT', route);
-};
+Route.prototype.put = function put (route) {
+  return this._addRoute('PUT', route)
+}
 
 // patch: Matches against patch requests
-Route.prototype.patch = function patch(route) {
-  return this._addRoute('PATCH', route);
-};
+Route.prototype.patch = function patch (route) {
+  return this._addRoute('PATCH', route)
+}
 
 // del: Matches against delete requests
-Route.prototype.del = function del(route) {
-  return this._addRoute('DELETE', route);
-};
+Route.prototype.delete = function _delete (route) {
+  return this._addRoute('DELETE', route)
+}
 
 // options: Matches against options requests
-Route.prototype.options = function options(route) {
-  return this._addRoute('OPTIONS', route);
-};
+Route.prototype.options = function options (route) {
+  return this._addRoute('OPTIONS', route)
+}
 
-module.exports = Router;
+module.exports = Router
